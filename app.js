f6d16263e1a4a3fb619ced69749ccf3c2ebed0e7
Atamaon <139767434+Atamaon@users.noreply.github.com>
2023-07-17 07:23:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_0blze9d", "template_lo4py9e", formData)
+    emailjs.send("service_mo79ctn", "template_l3302jr", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
